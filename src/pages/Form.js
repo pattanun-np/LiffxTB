7440ed71b3c48775b2089dc5013df97a5b88baae
pattanunNP <phattanun19@hotmail.com>
2020-06-01 10:50:07 +0700

    ad term

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -2,7 +2,7 @@ import React from "react";
 import useLiff from "./component/liff_hook";
 import Nav from "./component/Navbar";
 import "./component/styles/button.css";
-
+import "./component/styles/checkbox.css";
 const liffId = "1654260546-VwqZxy4o";
 
 function Form() {
@@ -22,7 +22,7 @@ function Form() {
             marginTop: "5px",
             borderRadius: 20,
             paddingBottom: 10,
-            hight: 400,
+            hight: "350px",
             width: "auto",
           }}
         >
@@ -39,13 +39,37 @@ function Form() {
           </figure>
           <div className="media">
             <div className="media-content">
-              <p
+              <tag
+                className="tag is-success"
                 style={{
                   fontFamily: "Kanit",
-                  padding: "10px",
+                  fontSize: "14px",
+                  margin: "8px",
+                  color: "color",
                 }}
               >
-                กำลังพัฒนา....
+                คำนิยาม
+              </tag>
+              <span style={{ fontFamily: "Kanit", fontWeight: "bold" }}>
+                แบบคัดกรองวัณโรคปอด
+              </span>
+              <p
+                className="title"
+                style={{
+                  fontFamily: "Kanit",
+                  backgroundColor: "rgba(250,250,250.1)",
+                  fontSize: "14px",
+                  padding: "15px",
+                  color: "grey",
+                }}
+              >
+                ในเว็บแอปพลิเคชัน <span></span> บอทน้อยดิจิตอลด๊อกเตอร์
+                พัฒนาโดยสำนักงานป้องกันควบคุมโรคที่ 9 จังหวัดนครราชสีมา
+                เป็นเครื่องมือที่ใช้ในการคัดกรองเพื่อค้นหาวัณโรคปอดจากอาการสงสัย
+                อ้างอิงมาจากคู่มือการคัดกรองเพื่อค้นหาวัณโรคและวัณโรคดื้อยาของสำนักวัณโรค
+                กรมควบคุมโรค หากท่านทำแบบคัดกรองนี้แล้ว
+                ผลระบุว่าเสี่ยงต่อการเป็นวัณโรค
+                โปรดป้องกันการแพร่กระจายเชื้อตามคำแนะนำและรีบไปพบแพทย์ทันทีเพื่อรับการตรวจอย่างละเอียดต่อไป
               </p>
             </div>
           </div>
@@ -53,18 +77,54 @@ function Form() {
             <div className="level-item has-text-centered">
               {/* btn_start */}
               <div className="media">
+                <div className="media-left" style={{ paddingLeft: "10px" }}>
+                  <label className="checkbox">
+                    <input type="checkbox2" />
+                    <span
+                      style={{
+                        marginLeft: "5px",
+                        fontFamily: "Kanit",
+                        fontSize: "10px",
+                      }}
+                    >
+                      ยอมรับข้อกำหนดการใช้งาน
+                    </span>
+
+                    <a
+                      href="#"
+                      style={{
+                        marginLeft: "5px",
+                        fontFamily: "Kanit",
+                        fontSize: "10px",
+                      }}
+                    >
+                      คำนิยาม และ ข้อกำหนดการใช้งาน
+                    </a>
+                  </label>
+                </div>
+              </div>
+              <div
+                className="media"
+                style={{ marginLeft: "20px", marginRight: "20px" }}
+              >
                 <div className="media-content">
                   <button
                     style={{
                       fontFamily: "Kanit",
                       color: "white",
-                      marginTop: "210px",
+                      marginTop: "40px",
                       marginBottom: "5px",
                     }}
                     className="button-sub"
                   >
                     <i class="far fa-file-alt"></i>
-                    <span style={{ marginLeft: "10px" }}>
+                    <span
+                      style={{
+                        hight: "16px",
+                        width: "15px",
+                        marginLeft: "10px",
+                      }}
+                    >
                       เริ่มตอบแบบคัดกรอง
                     </span>
                   </button>
